3e8b2e7e8f7b9d7e35f8dba4630e7463ba607cec
Ming Quan <chua_ming_quan@live.com>
2020-06-29 18:40:45 +0800

    commit 3

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Auguest 27, 2018'
-    },
-    {
-        'author': 'Ming',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Auguest 29, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
